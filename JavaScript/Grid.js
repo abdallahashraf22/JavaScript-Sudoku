754b9924924151137c574e039c844094082a7c0e
Mohamed Hamdy <mohamedhamdy5281@gmail.com>
2022-01-29 23:10:24 +0200

    not more changes

---
+++ JavaScript/Grid.js
@@ -0,0 +1,6 @@
+let group = document.cookie.split(";")[0].split("=")[1] ; 
+let imgs = document.getElementsByTagName("li[class='cell']");
+console.log(imgs);
+for (let i = 0 ; i<imgs.length ; i++){
+  
+}
